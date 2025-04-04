83cb09ebb9a3e7080e2f1500e890cb549ac2763a
Raahguu <raahguu@outlook.com>
2025-04-04 23:13:42 +0800

    Bug fixed nav.js and footer.js

--- root/js/footer.js
+++ root/js/footer.js
@@ -2,6 +2,6 @@ fetch("/root/html/footer.html")
 .then((response) => response.text())
 .then((data) => { 
     let old_element = document.getElementById("replace_with_footer"); 
-    old_element.parentNode.replaceChild(new_element, data); 
+    old_element.parentNode.replaceChild(data, old_element); 
 })
 .catch((err) => console.log(err));
