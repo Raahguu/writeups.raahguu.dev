f0052f48101bce3df7df7e5a4cd71205cb03eb85
Raahguu <raahguu@outlook.com>
2025-04-04 21:31:24 +0800

    Added a nav bar and About page

---
+++ root/js/nav.js
@@ -0,0 +1,8 @@
+fetch("/root/html/nav.html")
+.then((response) => response.text())
+.then((data) => { 
+    let old_element = document.querySelector( "script#replace-with-navbar" ); 
+    let new_element = new DOMParser().parseFromString(data, "text/html").querySelector("nav"); 
+    old_element.parentNode.replaceChild(new_element, old_element); 
+})
+.catch((err) => console.log(err));
