3a4ece311f7dfa25e8cdf505982dd2213ecd5174
Raahguu <raahguu@outlook.com>
2025-04-04 22:53:33 +0800

    Fixed incorrect naming coventions in .js files

--- root/js/footer.js
+++ root/js/footer.js
@@ -1,7 +1,7 @@
 fetch("/root/html/footer.html")
 .then((response) => response.text())
 .then((data) => { 
-    let old_element = document.getElementById("replace-with-footer"); 
+    let old_element = document.getElementById("replace_with_footer"); 
     let new_element = new DOMParser().parseFromString(data, "text/html").querySelector("footer"); 
     old_element.parentNode.replaceChild(new_element, old_element); 
 })
--- root/js/nav.js
+++ root/js/nav.js
@@ -1,8 +1,8 @@
 fetch("/root/html/nav.html")
 .then((response) => response.text())
 .then((data) => { 
-    let old_element = document.getElementById("replace-with-navbar"); 
-    let new_element = new DOMParser().parseFromString(data, "text/html").querySelector("nav"); 
+    let old_element = document.getElementById("replace_with_navbar"); 
+    let new_element = new DOMParser().parseFromString(data, "text/html").querySelector("nav");
     old_element.parentNode.replaceChild(new_element, old_element); 
 })
 .catch((err) => console.log(err));
