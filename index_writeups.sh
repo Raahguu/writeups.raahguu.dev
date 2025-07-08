71fd4bcbed0bf2eb483857c6cf077689aef6cf93
Raahguu <raahguu@outlook.com>
2025-07-08 19:58:06 +0800

    Changed the 404 page and added arbitrary writeup-index.md file traversal

--- index_writeups.sh
+++ index_writeups.sh
@@ -3,10 +3,8 @@ for dir in $(find _writeups -type d); do
 	if [ "$dir" == "_writeups" ]; then
 		continue
 	fi
-	if [ ! -f "$dir/index.md" ]; then
-		echo "---" > "$dir/index.md"
-		echo "layout: writeup-index" >> "$dir/index.md"
-		echo "permalink: /${dir#_writeups/}/" >> "$dir/index.md"
-		echo "---" >> "$dir/index.md"
-	fi
+	echo "---" > "$dir/index.md"
+	echo "layout: writeup-index" >> "$dir/index.md"
+	echo "permalink: /${dir#_writeups/}/" >> "$dir/index.md"
+	echo "---" >> "$dir/index.md"
 done
